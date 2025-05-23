e2ccd8eee3c51e868bb0c144424568637c880cfe
muhab404 <83089239+muhab404@users.noreply.github.com>
2025-05-23 03:23:43 +0300

    first commit

--- handler.py
+++ handler.py
@@ -9,7 +9,7 @@ download_folder = "/tmp"
 
 def export_book_data(username, password, book_id, download_dir):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(accept_downloads=True)
         page = context.new_page()
 
