31ea4031afc9b6df8044c2dc8c75a98ea25e9a09
muhab404 <83089239+muhab404@users.noreply.github.com>
2025-05-23 04:02:04 +0300

    first commit

--- handler.py
+++ handler.py
@@ -66,7 +66,6 @@ def export_book_data(username, password, book_id, download_dir):
         # 10. Trigger download and wait for it
         with page.expect_download() as download_info:
             # Wait for the download button to be visible
-            page.wait_for_selector('a.show-loading.btn-mc-white-blue', state='visible', timeout=60000)
             screenshot_path = "/tmp/debug_before_download.png"
             page.screenshot(path=screenshot_path)
             # Upload to S3
@@ -75,7 +74,8 @@ def export_book_data(username, password, book_id, download_dir):
                 "playwright-lambda-fenction",  # Replace with your bucket name
                 f"debug/{book_id}_before_download.png"
             )
-            page.click('a.show-loading.btn-mc-white-blue', timeout=60000)  # Actual download button
+            page.wait_for_selector('a.show-loading.btn-mc-white-blue', state='visible', timeout=180000)
+            page.click('a.show-loading.btn-mc-white-blue', timeout=180000)  # Actual download button
 
         download = download_info.value
 
